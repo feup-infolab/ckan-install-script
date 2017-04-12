246d4c11c78c225bae41c557306ca2aaf722bd24
João Rocha da Silva <joaorosilva@gmail.com>
2017-04-12 13:44:32 +0100

    Update datastore_install.sh

--- datastore_install.sh
+++ datastore_install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-ckan_config_file"/etc/ckan/default/development.ini"
-datastore_plugin_activation_line = "ckan.plugins = datastore"
-grep -q -F 'datastore_plugin_activation_line' $ckan_config_file || echo 'datastore_plugin_activation_line"' >> $ckan_config_file
+#ckan_config_file"/etc/ckan/default/development.ini"
+#datastore_plugin_activation_line = "ckan.plugins = datastore"
+#grep -q -F 'datastore_plugin_activation_line' $ckan_config_file || echo 'datastore_plugin_activation_line"' >> $ckan_config_file
 
 #create datastore user and database
 sudo -u postgres createuser -S -D -R -P -l datastore_default
@@ -10,12 +10,12 @@ sudo -u postgres createdb -O ckan_default datastore_default -E utf-8
 
 #add datastore parameters to the config file
 
-datastore_write_url="ckan.datastore.write_url = postgresql://ckan_default:pass@localhost/datastore_default"
-grep -q -F 'datastore_plugin_activation_line' $ckan_config_file || echo 'datastore_plugin_activation_line"' >> $ckan_config_file
-datastore_read_url="ckan.datastore.read_url = postgresql://datastore_default:pass@localhost/datastore_default"
-grep -q -F 'datastore_plugin_activation_line' $ckan_config_file || echo 'datastore_plugin_activation_line"' >> $ckan_config_file
+#datastore_write_url="ckan.datastore.write_url = postgresql://ckan_default:pass@localhost/datastore_default"
+#grep -q -F 'datastore_plugin_activation_line' $ckan_config_file || echo 'datastore_plugin_activation_line"' >> $ckan_config_file
+#datastore_read_url="ckan.datastore.read_url = postgresql://datastore_default:pass@localhost/datastore_default"
+#grep -q -F 'datastore_plugin_activation_line' $ckan_config_file || echo 'datastore_plugin_activation_line"' >> $ckan_config_file
 
-#set permissions
+#activate environment
 su ckan
 virtualenv --no-site-packages /usr/lib/ckan/default
 . /usr/lib/ckan/default/bin/activate
