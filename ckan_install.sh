2b5458df81fd505901377d88a2a076965b3e9c78
João Rocha da Silva <joaorosilva@gmail.com>
2017-05-04 17:34:31 +0100

    Reviewed CKAN installation script
    Reviewed CKAN installation script

--- ckan_install.sh
+++ ckan_install.sh
@@ -7,66 +7,63 @@ die_on_bad_cd()
   #exit 1
 }
 
-sudo apt-get install python-dev postgresql libpq-dev python-pip python-virtualenv git-core solr-jetty
-sudo -H pip install --upgrade pip
+sudo apt-get install python-dev postgresql libpq-dev python-pip python-virtualenv git-core solr-jetty &&
+sudo -H pip install --upgrade pip &&
 
 #fixes UndefinedEnvironmentName: 'extra' does not exist in evaluation environment error on ubuntu 16.04
 # https://github.com/getsentry/sentry/issues/3143
-pip install setuptools==20.4
+sudo pip install setuptools==20.4
 
 #create ckan user
 sudo adduser ckan
 
-mkdir -p ~/ckan/lib
-sudo ln -s ~/ckan/lib /usr/lib/ckan
-mkdir -p ~/ckan/etc
-sudo ln -s ~/ckan/etc /etc/ckan
+mkdir -p ~/ckan/lib &&
+sudo ln -s ~/ckan/lib /usr/lib/ckan &&
+mkdir -p ~/ckan/etc &&
+sudo ln -s ~/ckan/etc /etc/ckan &&
 
-sudo mkdir -p /usr/lib/ckan/default
-sudo chown ckan /usr/lib/ckan/default
-sudo mkdir -p /var/lib/ckan/resources
-sudo chown ckan /var/lib/ckan
+sudo mkdir -p /usr/lib/ckan/default &&
+sudo chown ckan /usr/lib/ckan/default &&
+sudo mkdir -p /var/lib/ckan/resources &&
+sudo chown ckan /var/lib/ckan &&
 
-export LC_ALL="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8" &&
 export LC_CTYPE="en_US.UTF-8"
 
 sudo su ckan #enter password
 virtualenv --no-site-packages /usr/lib/ckan/default
 . /usr/lib/ckan/default/bin/activate
+pip install setuptools==20.4
 
 #deactivate
 
-cd ~ || die_on_bad_cd "$HOME"
-pip install -e 'git+https://github.com/ckan/ckan.git@ckan-2.5.2#egg=ckan'
+cd ~ || die_on_bad_cd "$HOME";
+pip install -e 'git+https://github.com/ckan/ckan.git@ckan-2.5.2#egg=ckan' &&
 pip install -r /usr/lib/ckan/default/src/ckan/requirements.txt --allow-all-external
 
 ##	for verification
 #deactivate
 #. /usr/lib/ckan/default/bin/activate
 
+exit #leave ckan user shell
+
 #check databases (must be utf8)
-sudo -u postgres psql -l
+#sudo -u postgres psql -l;
 
 #create postgresql user
-sudo -u postgres createuser -S -D -R -P ckan_default
+sudo -u postgres createuser -S -D -R -P ckan_default;
 
+#create CKAN database
 sudo -u postgres createdb -O ckan_default ckan_default -E utf-8
 
 #Create a CKAN config file
-sudo mkdir -p /etc/ckan/default
-sudo chown -R ckan /etc/ckan/
-sudo chown -R ckan ~/ckan/etc
-sudo chown -R ckan  ~/ckan/etc
-
-sudo apt-get -y install nodejs npm vim
-su ckan
-git clone https://github.com/silvae86/filepatcher.git
-cd filepatcher || die_on_bad_cd "$pwd/filepatcher"
-npm install
+sudo mkdir -p /etc/ckan/default &&
+sudo chown -R ckan /etc/ckan/ &&
+sudo chown -R ckan ~/ckan/etc &&
+sudo chown -R ckan  ~/ckan/etc &&
 
 #edit Jetty8 configuration
-chmod ugo+w /etc/default/jetty8
-vim /etc/default/jetty8
+sudo vim /etc/default/jetty8
 
 #Replace sections
 #   NO_START=0                            -> NO_START=0            # (line 4)
@@ -79,40 +76,33 @@ printf "Edit Jetty config file at /etc/default/jetty8"
 read
 
 #Replace the default schema.xml file with a symlink to the CKAN schema file included in the sources.
-sudo mv /etc/solr/conf/schema.xml /etc/solr/conf/schema.xml.bak
-sudo ln -s /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
-
+sudo mv /etc/solr/conf/schema.xml /etc/solr/conf/schema.xml.bak &&
+sudo ln -s /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml &&
 sudo service jetty8 restart
 
 #check if jetty runs at the correct port
-telnet localhost 8983
+telnet localhost 8983 #should show "Escape character is '^]'"
 
 #create ckan configuration file
-sudo apt-get install python-pastescript
+sudo apt-get -y install python-pastescript
+
 su ckan
 cd ~ || die_on_bad_cd "$HOME"
 
 #activate python virtualenv
-su ckan
 virtualenv --no-site-packages /usr/lib/ckan/default
 . /usr/lib/ckan/default/bin/activate
 
 #create default ckan config file
 paster make-config ckan /etc/ckan/default/development.ini
 
-#change solr url in ckan config file to solr_url=http://127.0.0.1:8983/solr
+#Replace sections in CKAN configuration
 vim /etc/ckan/default/development.ini
 
 #Replace sections
 #   #solr_url = http://127.0.0.1:8983/solr                            -> solr_url=http://127.0.0.1:8983/solr
-
-#save (type :wq ENTER)
-
-#edit CKAN site_url and default password
-vim /etc/ckan/default/development.ini
-
-#Replace sections
 #   ckan.site_url =                             -> ckan.site_url = http://<IP/name of the machine.xpto.com>
+#   #DB PASSWORD
 #   sqlalchemy.url = postgresql://ckan_default:pass@localhost/ckan_default =                             -> sqlalchemy.url = postgresql://ckan_default:ckan@localhost/ckan_default #ASSUMING THE postgresql credentials are: password 'ckan';  username 'ckan_default'
 
 #save (type :wq ENTER)
@@ -130,22 +120,27 @@ ln -s /usr/lib/ckan/default/src/ckan/who.ini /etc/ckan/default/who.ini
 cd /usr/lib/ckan/default/src/ckan || die_on_bad_cd "/usr/lib/ckan/default/src/ckan"
 paster serve /etc/ckan/default/development.ini
 
-#set up ckan service
-sudo touch /var/log/ckan_port_5000.log
-sudo chown ckan /var/log/ckan_port_5000.log
-sudo chmod 0666 /var/log/ckan_port_5000.log
+#check your new ckan installation in the browser...
 
-sudo rm -rf /etc/systemd/system/ckan_port_5000.service
-sudo touch /etc/systemd/system/ckan_port_5000.service
-sudo chmod 0655 /etc/systemd/system/ckan_port_5000.service
-sudo mkdir /var/lib/ckan
+#leave ckan user's shell
+exit
+
+#set up ckan service
+sudo touch /var/log/ckan_port_5000.log &&
+sudo chown ckan /var/log/ckan_port_5000.log &&
+sudo chmod 0666 /var/log/ckan_port_5000.log &&
+
+sudo rm -rf /etc/systemd/system/ckan_port_5000.service &&
+sudo touch /etc/systemd/system/ckan_port_5000.service &&
+sudo chmod 0655 /etc/systemd/system/ckan_port_5000.service &&
+sudo mkdir -p /var/lib/ckan &&
 
 #create storage files
-sudo mkdir -p /var/lib/ckan/resources
-sudo chmod u+rwx /var/lib/ckan/resources
+sudo mkdir -p /var/lib/ckan/resources &&
+sudo chmod u+rwx /var/lib/ckan/resources  &&
 
-sudo mkdir -p /var/lib/ckan/default
-sudo chmod u+rwx /var/lib/ckan/default
+sudo mkdir -p /var/lib/ckan/default  &&
+sudo chmod u+rwx /var/lib/ckan/default  &&
 
 sudo chown -R ckan /var/lib/ckan/
 
